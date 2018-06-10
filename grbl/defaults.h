edf53d0c294b65403eba01adb202d87cd66f67a8
Patrick Felixberger <robomaniac8@googlemail.com>
2018-06-10 23:31:55 +0200

    Changed x-accel

--- grbl/defaults.h
+++ grbl/defaults.h
@@ -36,7 +36,7 @@
   #define DEFAULT_X_MAX_RATE 				1000.0 // mm/min
   #define DEFAULT_Y_MAX_RATE 				1000.0 // mm/min
   #define DEFAULT_Z_MAX_RATE 				1000.0 // mm/min
-  #define DEFAULT_X_ACCELERATION 			(800.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
+  #define DEFAULT_X_ACCELERATION 			(20.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Y_ACCELERATION 			(20.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_Z_ACCELERATION 			(20.0*60*60) // 10*60*60 mm/min^2 = 10 mm/sec^2
   #define DEFAULT_X_MAX_TRAVEL 				400.0 // mm NOTE: Must be a positive value.

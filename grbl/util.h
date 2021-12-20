51a3cae056e7c46145e3f1557301b8c136ebf3d1
Patrick F <robomaniac8@googlemail.com>
2021-12-20 13:13:29 +0100

    Bugfix corexy #80

--- grbl/util.h
+++ grbl/util.h
@@ -90,10 +90,8 @@
 
 // CoreXY motor assignments. DO NOT ALTER.
 // NOTE: If the A and B motor axis bindings are changed, this effects the CoreXY equations.
-#ifdef COREXY
 #define A_MOTOR                    X_AXIS // Must be X_AXIS
 #define B_MOTOR                    Y_AXIS // Must be Y_AXIS
-#endif
 
 
 // Conversions
